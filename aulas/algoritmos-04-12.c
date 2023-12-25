9715a81fe0efc42c177803b88c56dabb93429af7
Levítico Rimon <95304656+LEVEL303@users.noreply.github.com>
2023-12-25 18:53:33 +0000

    update

--- aulas/algoritmos-04-12.c
+++ aulas/algoritmos-04-12.c
@@ -17,11 +17,16 @@ int multiplicacao(int a, int b) {
 }
 
 int divisao(int a, int b) {
-    return a / b;
+    if(b != 0) {
+        return a / b;
+    } else {
+        printf("Não é possível dividir por 0\n");
+        return 0;
+    }
 }
 
 int calcula(int x, int y, int (*op)(int, int)) {
-    return (*op)(x, y);
+    return op(x, y);
 }
 
 int main() {
@@ -29,7 +34,7 @@ int main() {
     increment(&a);
     printf("%d\n", a);
     printf("===================\n");
-    int resultado = calcula(8, 2, soma);
-    printf("Resultado: %d", resultado);
+    int resultado = calcula(8, 0, divisao);
+    printf("Resultado: %d\n", resultado);
     return 0;
 }
