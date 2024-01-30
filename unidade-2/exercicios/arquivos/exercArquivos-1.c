836432ab2285bbd49a9d0e05e789dd305ccbb957
Levítico Rimon <95304656+LEVEL303@users.noreply.github.com>
2024-01-30 22:49:01 +0000

    update

--- unidade-2/exercicios/arquivos/exercArquivos-1.c
+++ unidade-2/exercicios/arquivos/exercArquivos-1.c
@@ -6,13 +6,14 @@
 
 typedef struct aluno {
     char nome[80];
-    char * notas;
-}Aluno;
+    float * notas;
+    float media;
+} Aluno;
 
 float calcularMedia(float * notas) {
     int i;
     float media = 0;
-    for(i = 0; i < QUANT_NOTAS; i++) {
+    for (i = 0; i < QUANT_NOTAS; i++) {
         media = media + notas[i];
     }
     return media / QUANT_NOTAS;
@@ -20,34 +21,38 @@ float calcularMedia(float * notas) {
 
 int main() {
     FILE * arq_entrada;
-    FILE * arq_saida;
     Aluno * alunos = (Aluno *) malloc(QUANT_ALUNOS * sizeof(Aluno));
     int i, j;
-    
-    for(i = 0; i < QUANT_ALUNOS; i++) {
-        alunos[i].notas = (char *) malloc(QUANT_NOTAS * sizeof(char)); 
+
+    for (i = 0; i < QUANT_ALUNOS; i++) {
+        alunos[i].notas = (float *) malloc(QUANT_NOTAS * sizeof(float));
     }
 
     arq_entrada = fopen("entrada.txt", "rt");
-    if(arq_entrada == NULL) {
+    if (arq_entrada == NULL) {
         printf("Erro ao abrir arquivo!\n");
         exit(1);
     } else {
         printf("Arquivo aberto com sucesso.\n");
     }
 
-    for(i = 0; i < QUANT_ALUNOS; i++){
+    for (i = 0; i < QUANT_ALUNOS; i++) {
         fscanf(arq_entrada, "%s", alunos[i].nome);
         printf("%s\n", alunos[i].nome);
-        for(j = 0; j < QUANT_NOTAS; j++) {
-            fscanf(arq_entrada, "%s", alunos[i].notas[j]);
-            printf("%s\n", alunos[i].notas[j]);
+        for (j = 0; j < QUANT_NOTAS; j++) {
+            fscanf(arq_entrada, "%f", &alunos[i].notas[j]);
+            printf("%.1f\n", alunos[i].notas[j]);
         }
     }
-    
-    if(fclose(arq_entrada) == 0) {
+
+    if (fclose(arq_entrada) == 0) {
         printf("Arquivo fechado com sucesso.\n");
     }
+
+    for (i = 0; i < QUANT_ALUNOS; i++) {
+        free(alunos[i].notas);
+    }
+    free(alunos);
 
     return 0;
 }
